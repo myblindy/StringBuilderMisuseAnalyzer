918db2c9203b3e775dfe91397d09aa60ecc6403f
Marius <tweety.04.01@gmail.com>
2023-10-09 17:01:04 -0400

    fix for `$$` in test

--- StringBuilderMisuseAnalyzer/StringBuilderMisuseAnalyzer.Test/Verifiers/CSharpCodeFixVerifier`2.cs
+++ StringBuilderMisuseAnalyzer/StringBuilderMisuseAnalyzer.Test/Verifiers/CSharpCodeFixVerifier`2.cs
@@ -52,6 +52,7 @@ namespace StringBuilderMisuseAnalyzer.Test
             {
                 TestCode = source,
                 FixedCode = fixedSource,
+                MarkupOptions = MarkupOptions.TreatPositionIndicatorsAsCode
             };
 
             test.ExpectedDiagnostics.AddRange(expected);
